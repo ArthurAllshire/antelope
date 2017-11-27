de5b1d729c8c156baaa03d5fe73a4d9db78114b3
Arthur Allshire <arthur.allshire@gmail.com>
2017-11-27 17:41:54 +1100

    Add stub FRC class

---
+++ frc.py
@@ -0,0 +1,12 @@
+from predict.elo import Elo
+from predict.sim import EventSimutator
+
+
+class FRC:
+
+    """Class to tie the rest of the event-getting and predicting stuff together
+    to separate it from the flask stuff. """
+    def __init__(self, year):
+        # load blue alliance key
+        with open('tba/key.txt', 'r') as keyfile:
+            self.tba_key = keyfile.readline().rstrip('\n')
