aa5ace825b21760c88d569dc68c2f9b93c5c1f9d
Arthur Allshire <arthur.allshire@gmail.com>
2017-11-27 18:02:07 +1100

    Add fetch_all_matches utility

---
+++ util/fetch_all_matches.py
@@ -0,0 +1,16 @@
+from tba_wrapper import BlueAllianceWrapper
+import pickle
+
+tba_key = None
+with open('../tba/key.txt', 'r') as keyfile:
+    tba_key = keyfile.readline().rstrip('\n')
+
+HEADERS = {'X-TBA-App-Id': 'Arthur Allshire:Antilope',
+           'X-TBA-Auth-Key': tba_key}
+
+tba_wrapper = BlueAllianceWrapper(HEADERS)
+
+for year in range(2008, 2018):
+    print(year)
+    matches = tba_wrapper.get_year_matches(str(year))
+    pickle.dump(matches, open('../cache/'+str(year)+'matches.p', 'wb'))
